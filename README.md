9495c703b24688c80137104c35c0cecae8d4d5a1
Porridge34 <ivy.yan.ng@gmail.com>
2021-12-05 17:54:15 -0500

    done with rps

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 To run this shit, u gotta use node deploycommands.js then node main.js
 also test.js is purely for testing purposes rn it is pretty useless dont freak out
+
+also im not sure how 2 fix but u need 2 change guild id in config.json for different servers
--- commands/rpc.js
+++ commands/rpc.js
@@ -1,23 +1,64 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rpc')
 		.setDescription('Play rpc'),
 	async execute(interaction) {
-		await printAfter();
-        await interaction.reply('P!');
-        await interaction.reply('C!');
-        await interaction.reply('S!');
-	},
-	function: printAfter()
+		await printAfter(1000, 'R!', interaction, false);
+        await printAfter(1000, 'P!', interaction, true);
+		await printAfter(1000, 'C!', interaction, true);
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('0')
+					.setLabel('rock')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('1')
+					.setLabel('paper')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('2')
+					.setLabel('scissor')
+					.setStyle('PRIMARY'),
+			);
+		await printAfter(1000, { content: 'S!', components: [row] }, interaction, true);
+		const filter = i => ((i.customId === '1' || i.customId === '0' || i.customId === '2') && i.member == interaction.member);
+
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		collector.on('collect', async i => {
+			// calc win or loss here (33% for every outcome)
+			tempInt = Math.floor(Math.random() * 3)
+			if (tempInt == 0) {
+				await i.update({ content: 'I logd', components: [] });
+			}
+			else if (tempInt == 1) {
+				await i.update({ content: 'I wind', components: [] });
+			}
+			else {
+				await i.update({ content: 'We must battle again! (run the command again because its a tie i cba to make a while loop)', components: [] });
+			}
+			return
+		});
+
+		collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+	}
 };
 
-function printAfter(time, message, interaction){
+function printAfter(time, message, interaction, replied){
 	const myPromise = new Promise((resolve, reject) => {
 		setTimeout(() => {
-			interaction.reply(message)
+			if (replied == true) {
+				interaction.followUp(message)
+			}
+			else {
+				interaction.reply(message)
+			}
 			resolve('foo');
 		}, time);
 	  });
+	return myPromise
 }
